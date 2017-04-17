73562d6461316857c53f3427315fb82f007034a5
Bill Sun <billsun@usc.edu>
2017-04-17 15:29:40 -0700

    Add controller to access DataApi service
    Implemented using AutoRest generated client library

---
+++ NonStandardSessionRequestForm/Controllers/Api/SessionRequestController.cs
@@ -0,0 +1,43 @@
+﻿using DataApi;
+using DataApi.Models;
+using Microsoft.Rest;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Web.Http;
+
+namespace USC.RNR.NonStandardSessionRequestForm.Controllers.Api
+{
+    [RoutePrefix("api")]
+    public class SessionRequestController : ApiController
+    {        
+        [Route("sessionrequests")]
+        public IHttpActionResult Post(Session session)
+        {
+            try
+            {
+                using (var client = new DataApiClient(new Uri(ConfigurationManager.AppSettings["DataApiUrl"])))
+                {
+                    client.SessionRequest.PostBysessionDTO(session);
+                }
+            }
+            catch (HttpOperationException apiEx)
+            {
+                Log.Logger.Error("Failed to POST session! Error: {Error}", apiEx.Message);
+            }
+            catch (Exception ex)
+            {
+                Log.Logger.Error("Failed to POST session! Error: {Error}", ex.Message);
+            }
+
+            return Ok();
+        }
+
+        [Route("sessionrequests/{requestId}")]
+        public IHttpActionResult Get(int requestId)
+        {
+            return Ok();
+        }
+    }
+}
