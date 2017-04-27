e2218331e74072b5b2fa7def414283227ef7fbc7
Sonny Duque <anthondd@usc.edu>
2017-04-27 08:39:43 -0700

    rateTableService: - added full path of the service because it seems like without the path, it would work on the local but not once published.

--- NonStandardSessionRequestForm/Scripts/app/services/rateTableService.js
+++ NonStandardSessionRequestForm/Scripts/app/services/rateTableService.js
@@ -3,7 +3,7 @@
 sessionModule.factory('RateTable', ['$resource', function ($resource) {
 
     return $resource(
-        '/api/ratetable', null,
+        window.location.origin + window.location.pathname + "api/ratetable", null,
         { "query": { method: "GET", isArray: true} }
     );
 
