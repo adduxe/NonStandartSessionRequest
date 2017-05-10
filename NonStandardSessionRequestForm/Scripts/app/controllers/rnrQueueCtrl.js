5b3df19672f77b35c5502e4c449489f03f509724
Sonny Duque <anthondd@usc.edu>
2017-05-10 09:02:01 -0700

    Created service for Session Codes.

--- NonStandardSessionRequestForm/Scripts/app/controllers/rnrQueueCtrl.js
+++ NonStandardSessionRequestForm/Scripts/app/controllers/rnrQueueCtrl.js
@@ -93,16 +93,19 @@
 
     function getRateTypeDescription(rateTypeCode) {
 
-        var rateTypes = [   // Rate type lookup table
-            { rateCode: "STD", rateName: "Standard (001)" },
-            { rateCode: "GB", rateName: "Graduate Business" },
-            { rateCode: "GCA", rateName: "Graduate Cinematic Arts" },
-            { rateCode: "GE", rateName: "Graduate Engineering" },
-            { rateCode: "DT3", rateName: "Dentistry" },
-            { rateCode: "AD3", rateName: "Advanced Dentistry" },
+        var rateTypes =[
+            { rateCode: "STD",  rateName: "Standard (session 001)" },
+            { rateCode: "GBUS", rateName: "Graduate Business" },
+            { rateCode: "GCINA",rateName: "Graduate Cinematic Arts" },
+            { rateCode: "GENGR",rateName: "Graduate Engineering" },
+            { rateCode: "MRED", rateName: "Master of Real Estate Development"},
+            { rateCode: "PHAR", rateName: "Pharmacy" },
+            { rateCode: "DENT", rateName: "Dentistry"},
+            { rateCode: "DH",   rateName: "Dental Hygiene" },
+            { rateCode: "ADVDE",rateName: "Advanced Dentistry"},
             { rateCode: "LAW", rateName: "Law" },
             { rateCode: "MED", rateName: "Medicine" },
-            { rateCode: "OTH", rateName: "Others" }
+            { rateCode: "OTH", rateName: "Others"}
         ];
 
         var rateDesc = "";
